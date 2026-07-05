a692d66329166ea4d0de2a040f3accb1d4c9a41c
SilentDemonSD <105407900+SilentDemonSD@users.noreply.github.com>
2026-07-05 13:42:27 +0200

    bump to v3.0.12, add custom brutalism CSS with Elms Sans font, full-width layout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,9 @@ extensions = [
 
 autosummary_generate = True
 
+napoleon_use_rtype = False
+napoleon_use_param = False
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "_includes", "Thumbs.db", ".DS_Store", "telegram/base", "telegram/functions", "telegram/types"]
 
@@ -30,6 +33,9 @@ html_theme = "furo"
 html_title = "wzgram"
 html_baseurl = "https://rjriajul.github.io/wzgram/"
 html_static_path = ["_static"]
+html_css_files = [
+    "css/custom.css",
+]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
@@ -38,3 +44,5 @@ intersphinx_mapping = {
 autodoc_default_options = {
     "member-order": "bysource",
 }
+
+suppress_warnings = ["image.not_readable"]
